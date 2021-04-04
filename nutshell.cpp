8c0780d89c2905ae530a85a122c71fac3340f960
Michel Chalfoun <mikachalfoun@gmail.com>
2021-04-04 00:46:22 -0400

    Env implementation and colored text

--- nutshell.cpp
+++ nutshell.cpp
@@ -6,11 +6,14 @@
 #include "commands.h"
 #include <unistd.h>
 #include <iostream>
+#include <map>
 
 using namespace std;
 
 char *getcwd(char *buf, size_t size);
 int yyparse();
+
+map<string, string> environment;
 
 int handleCD(string newDir){
     char arr[FILENAME_MAX];
@@ -20,40 +23,57 @@ int handleCD(string newDir){
     if (newDir[0] == '/') { // arg is relative path
 		targetDir = newDir;
 	}
-    
+
     if(chdir(targetDir.c_str()) == 0) {
-        printf("Directory found\n");
+        environment["PROMPT"] = targetDir;
         return 1;
 		}
 	else {
         printf("Directory not found\n");
         return 1;
 	}
-    return 0;
+}
+
+int handleSETENV(string name, string value){
+    if ((name.length() > 0) && (value.length() > 0)){
+        environment[name] = value;
+        return 1;
+    }else{
+        printf("Invalid input.");
+        return 1;
+    }
+}
+
+int handlePRINTENV(){
+    for (auto i = environment.begin(); i != environment.end(); i++){
+        printf("%s:%s\n", (i->first).c_str(), (i->second).c_str());
+    }
+    return 1;
+}
+
+int handleUNSETENV(string name){
+    if (name.length() > 0){
+        environment.erase(name);
+        return 1;
+    }else{
+        printf("Invalid input.");
+        return 1;
+    }
 }
 
 int main()
 {
-    varIndex = 0;
     getcwd(cwd, sizeof(cwd));
 
-    strcpy(varTable.var[varIndex], "PWD");
-    strcpy(varTable.word[varIndex], cwd);
-    varIndex++;
-    strcpy(varTable.var[varIndex], "HOME");
-    strcpy(varTable.word[varIndex], cwd);
-    varIndex++;
-    strcpy(varTable.var[varIndex], "PROMPT");
-    strcpy(varTable.word[varIndex], cwd);
-    varIndex++;
-    strcpy(varTable.var[varIndex], "PATH");
-    strcpy(varTable.word[varIndex], ".:/bin");
-    varIndex++;
+    environment["PWD"] = cwd;
+    environment["HOME"] = cwd;
+    environment["PROMPT"] = cwd;
+    environment["PATH"] = ".:/bin";
 
     system("clear");
     while(1)
     {
-        printf("[%s]>> ", varTable.word[2]);
+        printf("%s [%s] >> %s", GREEN, environment["PROMPT"].c_str(), RESET);
         yyparse();
     }
 
